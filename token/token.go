0cfa4b38b593d36ac60f12e6ad83b10d37efb6b9
muiscript <myam0118@gmail.com>
2019-06-07 20:26:30 +0900

    make type of token type constants token.Type explicitly

--- token/token.go
+++ token/token.go
@@ -7,28 +7,32 @@ type Token struct {
 	Literal string
 }
 
+func New(tokenType Type, literal string) Token {
+	return Token{Type: tokenType, Literal: literal}
+}
+
 const (
-	ILLEGAL = "ILLEGAL"
-	EOR     = "EOF"
+	ILLEGAL Type = "ILLEGAL"
+	EOR     Type = "EOF"
 
 	// identifier & literals
-	IDENTIFIER = "IDENTIFIER"
-	INT        = "INT"
+	IDENTIFIER Type = "IDENTIFIER"
+	INT        Type = "INT"
 
 	// operators
-	ASSIGN = "="
-	PLUS   = "+"
+	ASSIGN Type = "="
+	PLUS   Type = "+"
 
 	// delimiters
-	COMMA     = ","
-	SEMICOLON = ";"
+	COMMA     Type = ","
+	SEMICOLON Type = ";"
 
-	LPAREN = "("
-	RPAREN = ")"
-	LBRACE = "{"
-	RBRACE = "}"
+	LPAREN Type = "("
+	RPAREN Type = ")"
+	LBRACE Type = "{"
+	RBRACE Type = "}"
 
 	// keywords
-	FUNCTION = "FUNCTION"
-	LET      = "LET"
+	FUNCTION Type = "FUNCTION"
+	LET      Type = "LET"
 )
