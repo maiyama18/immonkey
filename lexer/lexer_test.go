20f45a6129be348a86778539a4aa687df5f78714
muiscript <myam0118@gmail.com>
2019-06-07 20:38:17 +0900

    make test for NextToken() TDT

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -8,22 +8,30 @@ import (
 )
 
 func TestLexer_NextToken(t *testing.T) {
-	input := `=+(){},;`
-	l := New(input)
-
-	expectedTokens := []token.Token{
-		{token.ASSIGN, "="},
-		{token.PLUS, "+"},
-		{token.LPAREN, "("},
-		{token.RPAREN, ")"},
-		{token.LBRACE, "{"},
-		{token.RBRACE, "}"},
-		{token.COMMA, ","},
-		{token.SEMICOLON, ";"},
+	tests := []struct {
+		input          string
+		expectedTokens []token.Token
+	}{
+		{
+			input: `=+(){},;`,
+			expectedTokens: []token.Token{
+				{token.ASSIGN, "="},
+				{token.PLUS, "+"},
+				{token.LPAREN, "("},
+				{token.RPAREN, ")"},
+				{token.LBRACE, "{"},
+				{token.RBRACE, "}"},
+				{token.COMMA, ","},
+				{token.SEMICOLON, ";"},
+			},
+		},
 	}
 
-	for _, expected := range expectedTokens {
-		actual := l.NextToken()
-		require.Equal(t, expected, actual)
+	for _, test := range tests {
+		l := New(test.input)
+		for _, expected := range test.expectedTokens {
+			actual := l.NextToken()
+			require.Equal(t, expected, actual)
+		}
 	}
 }
